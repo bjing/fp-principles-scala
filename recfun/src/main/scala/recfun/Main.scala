8485b56f0f1ed3db3a98371a74462acd5112e0d0
Brian Jing <brian.jing@myob.com>
2016-07-14 19:05:03 +1000

    Added solutions to week 1 exercises 1 and 2

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -13,12 +13,39 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
-  
+    def pascal(c: Int, r: Int): Int =
+      c match {
+        case 0 => 1
+        case `r` => 1
+        case _ => pascal(c-1, r-1) + pascal(c, r-1)
+      }
+
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+
+      def countBalance(chars: List[Char]): Boolean =
+        chars.count(_ == '(') == chars.count(_ == ')')
+
+      def orderBalance(chars: List[Char]): Boolean = {
+
+        def checkOrder(acc: List[Char], chars: List[Char]): Boolean = {
+          chars match {
+            case Nil => true
+            case h::rest => h match {
+              case '(' => checkOrder(h::acc, rest)
+              case ')' => if (acc.nonEmpty && acc.head == '(') checkOrder(acc.tail, rest) else false
+              case _ => checkOrder(acc, rest)
+            }
+          }
+        }
+
+        checkOrder(List[Char](), chars)
+      }
+
+    countBalance(chars) && orderBalance(chars)
+  }
   
   /**
    * Exercise 3
